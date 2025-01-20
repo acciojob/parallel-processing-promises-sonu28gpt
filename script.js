c079ed126c148dcc6e4972f847be80a58bfa5154
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-20 17:55:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+output.innerHTML=`<div id='loading'>Loading...</div>`
+
+function loadImage(image) {
+  return new Promise((resolve, reject) => {
+    let img = new Image();
+    img.src = image.url;
+
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+  });
+}
+function downloadImages(images) {
+  let promises = images.map(image => loadImage(image));
+
+  return Promise.all(promises);
+}
+
+downloadImages(images)
+  .then(imgElements => {
+    let output = document.getElementById('output');
+	  output.innerHTML='';
+    imgElements.forEach(img => output.appendChild(img));
+  })
+  .catch(error => {
+    let errorDiv = document.createElement('div');
+	  errorDiv.setAttribute('id','error')
+	  
+	    errorDiv.textContent = error;
+	  output.innerHTML='';
+	  output.appendChild(errorDiv);
+  });
